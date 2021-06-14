1dae01628bfe3abd9577a3d73e02e55b2d6ac1d2
Kristijan Prematarov <kristijanprematarov@yahoo.com>
2021-06-14 15:14:10 +0200

    initial

---
+++ Globomantics/Controllers/ConferenceController.cs
@@ -0,0 +1,42 @@
+﻿using Globomantics.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globomantics.Controllers
+{
+    public class ConferenceController : Controller
+    {
+        private readonly IConferenceService _conferenceService;
+
+        public ConferenceController(IConferenceService conferenceService)
+        {
+            _conferenceService = conferenceService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Title = "Conferences";
+            return View(await _conferenceService.GetAll());
+        }
+
+        public IActionResult Add()
+        {
+            ViewBag.Title = "Add Conference";
+            return View(new ConferenceModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ConferenceModel model)
+        {
+            if (ModelState.IsValid)
+                await _conferenceService.Add(model);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
